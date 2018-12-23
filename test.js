3fc588428ed2da0839f7c0b246ab8946b88fc368
unbiarirang <unbiarirang@naver.com>
2018-12-23 16:27:39 +0800

    Complete lexer/parser

---
+++ test.js
@@ -0,0 +1,17 @@
+const compiler = require('./compiler');
+    fs = require('fs');
+
+function compileFile(filename) {
+    let text = fs.readFileSync(filename).toString();
+    console.log(text);
+    let tokens = compiler.tokenizer(text);
+    console.log(tokens);
+    let ast = compiler.parser(tokens);
+    console.log(ast);
+};
+
+process.argv.forEach(function(val) {
+    console.log(val);
+    if (val.slice(-2) == ".c")
+        compileFile(val);
+});
